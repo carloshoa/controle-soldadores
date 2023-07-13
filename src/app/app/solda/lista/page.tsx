a6e33772ffb9ccc5e7f3d72523ed7ca273ec36bc
Carlos Arantes <carlosarantes@CARLOS.localdomain>
2023-07-13 11:00:27 -0300

    adjusts loading

--- src/app/app/solda/lista/page.tsx
+++ src/app/app/solda/lista/page.tsx
@@ -194,8 +194,7 @@ const App = async () => {
                         {welder.rg}
                       </td>
                       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {console.log(welder.processos, ' ', typeof (welder.processos), ' ', welder.processos.join(' '))}
-                        {welder.processos}
+                        {welder.processos.join(' ')}
                       </td>
                       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         {welder.ativo}
